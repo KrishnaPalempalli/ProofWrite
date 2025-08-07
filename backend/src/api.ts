f279f7fd5dad41cdcc1538febac0d4b62c513223
Anton Angeletti <anton.angeletti@pinata.cloud>
2025-08-07 12:01:03 -0500

    GET document endpoint

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -21,37 +21,54 @@ router.get('/hello', async (req: Request, res: Response) => {
   res.send(`Hello World!<br>${JSON.stringify(app.locals.data)}`)
 })
 
-router.post('/doc/upload', async (req: Request, res: Response) => {
-  if ('name' in req.body && 'text' in req.body) {
-    // name is the name of the doc, text is the content
-    const name = req.body.name
-    const text = req.body.text
-    const timestamp = Date.now()
-    const file = new File([text], `HACKATHON ${name}`, { type: 'text/plain' })
-    try {
-      const upload = await pinata.upload.private.file(file)
-      const cid = upload.cid
-      if (!app.locals.data[name]) {
-        app.locals.data[name] = []
-      }
-      const commit: DocumentCommit = { text, timestamp, cid }
-      if (!('is_duplicate' in upload && upload.is_duplicate)) {
-        app.locals.data[name].push(commit)
-      }
-      console.log(`uploaded file ${name} at ${timestamp}`, app.locals.data[name])
-      res.status(200).send({
-        message: 'successfully uploaded',
-        cid,
-      })
-    } catch (error) {
-      res.status(400).json({
-        message: 'failed to upload to pinata',
-        error: `${error}`,
-      })
-    }
+router.get('/doc', async (req: Request, res: Response) => {
+  if (!('name' in req.query)) {
+    return res.status(400).json({
+      message: 'bad request, fix your parameters dingus',
+    })
+  }
+  const name = req.query.name as string
+  if (name in app.locals.data) {
+    res.status(200).json({
+      message: 'successfully retrieved file history',
+      history: app.locals.data[name],
+    })
   } else {
+    res.status(404).json({
+      message: 'file not found',
+    })
+  }
+})
+
+router.post('/doc/upload', async (req: Request, res: Response) => {
+  if (!('name' in req.body && 'text' in req.body)) {
     res.status(400).json({
       message: 'bad request, fix your parameters dingus',
+    })
+  }
+  // name is the name of the doc, text is the content
+  const { name, text } = req.body
+  const timestamp = Date.now()
+  const file = new File([text], `HACKATHON ${name}`, { type: 'text/plain' })
+  try {
+    const upload = await pinata.upload.private.file(file)
+    const cid = upload.cid
+    if (!app.locals.data[name]) {
+      app.locals.data[name] = []
+    }
+    const commit: DocumentCommit = { text, timestamp, cid }
+    if (!('is_duplicate' in upload && upload.is_duplicate)) {
+      app.locals.data[name].push(commit)
+    }
+    console.log(`uploaded file ${name} at ${timestamp}`, app.locals.data[name])
+    res.status(200).send({
+      message: 'successfully uploaded',
+      cid,
+    })
+  } catch (error) {
+    res.status(400).json({
+      message: 'failed to upload to pinata',
+      error: `${error}`,
     })
   }
 })
