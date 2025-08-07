09ab6668d28fc7e6ee7a531e12d889d4194f51b2
Anton Angeletti <anton.angeletti@pinata.cloud>
2025-08-07 11:07:55 -0500

    pinata uploads!!

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { Router } from 'express'
 import { app } from '..'
 import { config } from 'dotenv'
+import { PinataSDK } from 'pinata'
 
 type DocumentCommit = {
   text: string
@@ -10,6 +11,10 @@ type DocumentCommit = {
 }
 
 config()
+const pinata = new PinataSDK({
+  pinataJwt: process.env.PINATA_JWT,
+  pinataGateway: process.env.PINATA_GATEWAY,
+})
 const router = Router()
 
 router.get('/hello', async (req: Request, res: Response) => {
@@ -22,18 +27,27 @@ router.post('/doc/upload', async (req: Request, res: Response) => {
     const name = req.body.name
     const text = req.body.text
     const timestamp = Date.now()
-    const cid = 'TODO'
-    const data = app.locals.data
-    if (!data[name]) {
-      data[name] = []
+    const file = new File([text], `HACKATHON ${name}`, { type: 'text/plain' })
+    try {
+      const upload = await pinata.upload.private.file(file)
+      const cid = upload.cid
+      const data = app.locals.data
+      if (!data[name]) {
+        data[name] = []
+      }
+      const commit: DocumentCommit = { text, timestamp, cid }
+      data[name].push(commit)
+      console.log(`uploaded file ${name} at ${timestamp}`, data)
+      res.status(200).send({
+        message: 'successfully uploaded',
+        cid,
+      })
+    } catch (error) {
+      res.status(400).json({
+        message: 'failed to upload to pinata',
+        error: `${error}`,
+      })
     }
-    const commit: DocumentCommit = { text, timestamp, cid }
-    data[name].push(commit)
-    console.log(data)
-    res.status(200).send({
-      message: 'successfully uploaded',
-      cid,
-    })
   } else {
     res.status(400).json({
       message: 'bad request, fix your parameters dingus',
