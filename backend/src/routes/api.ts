282bbe73d5dcff8f8ec66a267f23235ed9fa5abb
Anton Angeletti <anton.angeletti@pinata.cloud>
2025-08-07 10:36:46 -0500

    api setup

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -2,70 +2,41 @@ import { Request, Response } from 'express'
 import { Router } from 'express'
 import { app } from '..'
 
+type DocumentCommit = {
+  text: string
+  timestamp: number // unix ms
+  cid: string
+}
+
 const router = Router()
 
-router.get('/hello', async (req, res) => {
+router.get('/hello', async (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
-// router.post('/interrogate', async (req: Request, res: Response) => {
-//   if (
-//     'name' in req.body &&
-//     'game_id' in req.body &&
-//     'message' in req.body &&
-//     'our_roll' in req.body &&
-//     'sus_roll' in req.body
-//   ) {
-//     const game = app.locals.game
-//     const gameUUID: string = req.body.game_id
-//     const suspectName: string = req.body.name.toLowerCase()
-//     const roll: number = req.body.our_roll
-//     const message: string = req.body.message
-//     if (!(gameUUID in game && suspectName in game[gameUUID])) {
-//       res.status(400).json({
-//         message: 'invalid gameUUID and/or suspectName',
-//         confidence: 0,
-//       })
-//     } else {
-//       try {
-//         const response = await getCharacterResponse(gameUUID, suspectName, message, roll)
-//         res.status(200).json(response)
-//       } catch (error) {
-//         console.error(error)
-//         res.status(400).json({
-//           message: 'error, check the console',
-//           confidence: 0,
-//         })
-//       }
-//     }
-//   } else {
-//     res.status(400).json({
-//       message: 'bad usage, check the json body',
-//       confidence: 0,
-//     })
-//   }
-// })
-
-// router.post('/start', async (req: Request, res: Response) => {
-//   let resp
-//   try {
-//     resp = await startGame(app.locals.suspects)
-//     res.status(200)
-//   } catch (error) {
-//     console.error(error)
-//     res.status(400)
-//   }
-//   res.json(resp)
-// })
-
-// router.post('/clear', async (req: Request, res: Response) => {
-//   if ('gameUUID' in req.body) {
-//     const gameUUID: string = req.body.gameUUID
-//     delete app.locals.game[gameUUID]
-//     res.status(200)
-//   } else {
-//     res.status(400)
-//   }
-// })
+router.post('/doc/upload', async (req: Request, res: Response) => {
+  if ('name' in req.body && 'text' in req.body) {
+    // name is the name of the doc, text is the content
+    const name = req.body.name
+    const text = req.body.text
+    const timestamp = Date.now()
+    const cid = 'TODO'
+    const data = app.locals.data
+    if (!data[name]) {
+      data[name] = []
+    }
+    const commit: DocumentCommit = { text, timestamp, cid }
+    data[name].push(commit)
+    console.log(data)
+    res.status(200).send({
+      message: 'successfully uploaded',
+      cid,
+    })
+  } else {
+    res.status(400).json({
+      message: 'bad request, fix your parameters dingus',
+    })
+  }
+})
 
 export { router }
